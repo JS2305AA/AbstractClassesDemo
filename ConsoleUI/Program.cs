c34d38e391041afeb0daa26ee90a7cc23edcd558
2305 <npjt2305@yahoo.com>
2022-11-11 08:58:17 -0500

    CompletedExercse

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -32,10 +32,36 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
+
+            var focus = new Car() { HasTrunk = true, Make = "Ford", Model = "Focus", Year = 2013 };
+
+            var motorcycle = new Motorcycle() { HasSideCart = true, Make = "HD", Model = "Chopper", Year = 1978 };
+
+            Vehicle sedan = new Car() { Make = "Dodge", Model = "Blah", Year = 2019 };
+
+            Vehicle sport = new Car() { HasTrunk = false, Make = "Ferrari", Model = "Something", Year = 2000 };
+
+            vehicles.Add(focus);
+            vehicles.Add(motorcycle);
+            vehicles.Add(sedan);
+            vehicles.Add(sport);
+
+            foreach(var veh in vehicles)
+            {
+                Console.WriteLine($"Make {veh.Make} Model {veh.Model} Year {veh.Year}");
+
+                veh.DriveAbstract();
+
+                Console.WriteLine("-----------");
+            }
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
-             * - new it up as one of each derived class
+             * - new it up as one of each derived class//
+             
+            
+             
              * Set the properties with object initializer syntax
              */
 
